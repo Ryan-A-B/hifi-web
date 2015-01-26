4fd9d1662626630831871f80ed8d8bd7dd25d9c7
Ryan Ballinger <ryan.a.ballinger@gmail.com>
2015-01-26 23:25:34 +0000

    Put everything in the correct folders

---
+++ names.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('config.php');
+
+    View::$title = 'High Fidelity Place Names';
+    View::write('page', 'names');
+
+    View::addStyle('css/names.css');
+
+    View::addScript('js/names.js');
+    View::addScript('js/profanityFilter.js');
+
+    View::renderCommonLayout('names');
+?>
